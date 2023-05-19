736b24de88d24a0d03b19b042b26236578c89fff
milton.gutierrez <milton.gutierrez@meta-dao.com>
2023-05-19 04:43:14 -0400

    update routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { createBrowserRouter, RouterProvider, } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import HomePage from './pages/home';
 import YouthPage from './pages/youth';
@@ -9,40 +9,19 @@ import Connect_1Page from './pages/connect_1';
 import Connect_2Page from './pages/connect_2 ';
 import RadioPage from './pages/radio';
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: (<HomePage />),
-  },
-  {
-    path: "/Quienes-somos",
-    element: (<AboutUsPage />),
-  },
-  ,
-  {
-    path: "/Elim-radio",
-    element: (<RadioPage />),
-  },
-  ,
-  {
-    path: "/jorge-Funetes",
-    element: (<Connect_1Page />),
-  },
-  ,
-  {
-    path: "/Noe-vargas",
-    element: (<Connect_2Page />),
-  },
-  ,
-  {
-    path: "/elim-jovenes",
-    element: (<YouthPage />),
-  },
-]);
-function App()
-{
+function App () {
   return (
-    <RouterProvider router={router} />
+    <Router>
+      <Routes>
+        <Route path="/" element={ <HomePage /> }> </Route>
+        <Route path="/Quienes-somos" element={ <AboutUsPage /> }> </Route>
+        <Route path="/Elim-radio" element={ <RadioPage /> }> </Route>
+        <Route path="/jorge-Funetes" element={ <Connect_1Page /> }> </Route>
+        <Route path="/Noe-vargas" element={ <Connect_2Page /> }> </Route>
+        <Route path="/elim-jovenes" element={ <YouthPage /> }> </Route>
+      </Routes>
+    </Router>
+
   );
 }
 
